d2e661fd1b9110efcbee41605e55b1cd3182ac0f
wcw <344078971@qq.com>
2023-12-19 00:23:47 +0800

    chore: parse number

--- javascript/parse.js
+++ javascript/parse.js
@@ -7,6 +7,60 @@ function parse_primitive(str, primitive, value) {
     else return PARSE_RESULT.INVALID_VALUE
 }
 
+function parse_number(str) {
+    const negative = str[0] === '-'
+    if (str[0] === '0' && isDigit(str[1])) return PARSE_RESULT.ROOT_NOT_SINGULAR
+
+    const [result, i] = parse_decimal(str, negative ? 1 : 0)
+    if (i === -1) return PARSE_RESULT.INVALID_VALUE
+
+    if (str[i] === 'e' || str[i] === 'E') {
+        const sign = str[i + 1] === '-' ? -1 : 1
+        const idx = isDigit(str[i + 1]) ? i + 1 : i + 2
+        let [exponent, _] = parse_decimal(str, idx)
+        exponent = sign * exponent
+
+        const res = (exponent === -324) ? Number.MIN_VALUE : result * Math.pow(10, exponent)
+        if (res === Infinity) return PARSE_RESULT.NUMBER_TOO_BIG
+        return negative && res !== 0 ? -1 * res : res
+    }
+    if (str.length > i) return PARSE_RESULT.ROOT_NOT_SINGULAR
+
+    return negative && result !== 0 ? -1 * result : result
+}
+
+function parse_decimal(str, idx) {
+    const [integer, i] = parse_int(str, idx)
+    if (str[i] === '.') {
+        if (!isDigit(str[i + 1])) return [0, -1]
+        const [frac, j] = parse_int(str, i + 1)
+        const result = integer + frac / 10 ** (j - i - 1)
+        return [result, j]
+    }
+    return [integer, i]
+}
+
+function parse_int(str, idx) {
+    let i = idx
+    while (isDigit(str[i])) i++
+
+    if (i === idx) return [0, i]
+
+    let s = 0
+    for (let j = i; j > idx; j--) {
+        s += 10 ** (i - j) * getDigit(str[j - 1])
+    }
+    return [s, i]
+}
+
+function isDigit(ch) {
+    return ch && ch.charCodeAt() >= '0'.charCodeAt() && ch.charCodeAt() <= '9'.charCodeAt()
+}
+
+function getDigit(ch) {
+    return ch.charCodeAt() - '0'.charCodeAt()
+}
+
 module.exports = function parse(str) {
     if (!str.trim()) return PARSE_RESULT.EXPECT_VALUE
 
@@ -18,7 +72,10 @@ module.exports = function parse(str) {
             return parse_primitive(str, 'true', true)
         case 'f':
             return parse_primitive(str, 'false', false)
+        case '-': return parse_number(str)
         default:
+            if (ch.charCodeAt() >= '0'.charCodeAt() && ch.charCodeAt() <= '9'.charCodeAt())
+                return parse_number(str)
             return PARSE_RESULT.INVALID_VALUE
     }
 }
