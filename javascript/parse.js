d108dc18279ad103a442461bd55f95a63abcf3e7
wcw <344078971@qq.com>
2023-12-22 00:44:34 +0800

    chore: parse unicode

--- javascript/parse.js
+++ javascript/parse.js
@@ -76,6 +76,13 @@ function parse_string(str) {
                 case 'f': string += "\f"; break;
                 case 't': string += "\t"; break;
                 case 'r': string += "\r"; break;
+                case 'u':
+                    const [decoded, j] = parse_unicode(str, i)
+                    if (j === -1) return PARSE_RESULT.INVALID_UNICODE_HEX
+                    if (j === -2) return PARSE_RESULT.INVALID_UNICODE_SURROGATE
+                    string += decoded
+                    i = j - 2
+                    break;
                 default: return PARSE_RESULT.INVALID_STRING_ESCAPE;
             }
             i += 2
@@ -86,6 +93,58 @@ function parse_string(str) {
         }
     }
     return string
+}
+
+function isHex(ch) {
+    return isDigit(ch)
+        || (ch.charCodeAt() >= 'a'.charCodeAt() && ch.charCodeAt() <= 'f'.charCodeAt())
+        || (ch.charCodeAt() >= 'A'.charCodeAt() && ch.charCodeAt() <= 'F'.charCodeAt())
+}
+
+function parse_unicode(str, idx) {
+    let unicode = ""
+    let j = idx;
+    while (str[j] === '\\' && str[j + 1] === 'u') {
+        unicode += str.slice(j + 2, j + 6)
+        j += 6
+    }
+
+    const nonHex = unicode.split('').filter(ch => !isHex(ch)).length
+    if (unicode.length % 4 || nonHex) return ['', -1]
+
+    const H = unicode.slice(0, 4).split('').reduce((s, ch, i) => s + getCharValue(ch) * 16 ** (4 - i - 1), 0)
+    const L = unicode.slice(4).split('').reduce((s, ch, i) => s + getCharValue(ch) * 16 ** (4 - i - 1), 0)
+
+    if (H >= 0xD800 && H <= 0xDBFF && (!L || L < 0xDC00 || L > 0xDFFF)) return ['', -2]
+
+    let val = H + L
+
+    const m = 0x80
+    if (val >= 0x000 && val <= 0x007f) {
+        return [String.fromCodePoint(val), j]
+    } else if (val >= 0x0080 && val <= 0x7ff) {
+        const b1 = 0xc0 | val >> 6 & 0x1f
+        const b2 = m | val & 0x3f
+        return [[b1, b2].map(v => String.fromCodePoint(v)).join(''), j]
+    } else if (val >= 0x0800 && val <= 0xffff) {
+        const b1 = 0xe0 | val >> 12 & 0xf;
+        const b2 = m | val >> 6 & 0x3f;
+        const b3 = m | val & 0x3f;
+        return [[b1, b2, b3].map(v => String.fromCodePoint(v)).join(''), j]
+    } else if (val >= 0x10000 && val <= 0x10ffff) {
+        val = 0x10000 + (H - 0xD800) * 0x400 + (L - 0xDC00)
+        const b1 = 0xf0 | val >> 18 & 0x7
+        const b2 = m | val >> 12 & 0x3f
+        const b3 = m | val >> 6 & 0x3f
+        const b4 = m | val & 0x3f
+        return [[b1, b2, b3, b4].map(v => String.fromCodePoint(v)).join(''), j]
+    }
+}
+
+function getCharValue(ch) {
+    if (isDigit(ch)) return getDigit(ch)
+    if (ch.charCodeAt() >= 'a'.charCodeAt()) return ch.charCodeAt() - 'a'.charCodeAt() + 10
+    else return ch.charCodeAt() - 'A'.charCodeAt() + 10
 }
 
 module.exports = function parse(str) {
